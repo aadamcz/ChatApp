d9dc3c7b0a29c0fc158fd4cf82793077e60f57af
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-02-05 20:27:03 +0100

    Modify styles, correct type error

--- client/App.js
+++ client/App.js
@@ -1,74 +1,71 @@
-import React, { Component } from 'react';
-import io from 'socket.io-client';
-import styles from './App.css';
+import React, { Component } from "react";
+import io from "socket.io-client";
+import styles from "./App.css";
 
-import MessageForm from './MessageForm';
-import MessageList from './MessageList';
-import UsersList from './UsersList';
-import UserForm from './UserForm';
+import MessageForm from "./MessageForm";
+import MessageList from "./MessageList";
+import UsersList from "./UsersList";
+import UserForm from "./UserForm";
 
-const socket = io('/');
+const socket = io("/");
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: [], 
-            messages: [], 
-            text: '', 
-            name: ''};
+            users: [],
+            messages: [],
+            text: "",
+            name: ""
+        };
     }
 
     componentDidMount() {
-        socket.on('message', message => this.messageReceive(message));
-        socket.on('update', ({users}) => this.chatUpdate(users));
+        socket.on("message", message => this.messageReceive(message));
+        socket.on("update", ({ users }) => this.chatUpdate(users));
     }
 
     messageReceive(message) {
         const messages = [message, ...this.state.messages];
-        this.setState({messages});
+        this.setState({ messages });
     }
 
     chatUpdate(users) {
-        this.setState({users});
+        this.setState({ users });
     }
 
     handleMessageSubmit(message) {
         const messages = [message, ...this.state.messages];
-        this.setState({message});
-        socket.emit('message', message);
+        this.setState({ messages });
+        socket.emit("message", message);
     }
 
     handleUserSubmit(name) {
-        this.setState({name});
-        socket.emit('join', name);
+        this.setState({ name });
+        socket.emit("join", name);
     }
-    
+
     render() {
-        return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
+        return this.state.name !== ""
+            ? this.renderLayout()
+            : this.renderUserForm();
     }
 
     renderLayout() {
         return (
             <div className={styles.App}>
                 <div className={styles.AppHeader}>
-                    <div className={styles.AppTitle}>
-                        ChatApp
-                    </div>
-                    <div className={styles.AppRoom}>
-                        App room
-                    </div>
+                    <div className={styles.AppTitle}>ChatApp</div>
+                    <div className={styles.AppRoom}>App room</div>
                 </div>
                 <div className={styles.AppBody}>
-                    <UsersList
-                        users={this.state.users}
-                    />
+                    <UsersList users={this.state.users} />
                     <div className={styles.MessageWrapper}>
-                        <MessageList
-                            messages={this.state.messages}
-                        />
-                        <MessageForm  
-                            onMessageSubmit={message => this.handleMessageSubmit(message)}
+                        <MessageList messages={this.state.messages} />
+                        <MessageForm
+                            onMessageSubmit={message =>
+                                this.handleMessageSubmit(message)
+                            }
                             name={this.state.name}
                         />
                     </div>
@@ -78,8 +75,8 @@ class App extends Component {
     }
 
     renderUserForm() {
-        return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
+        return <UserForm onUserSubmit={name => this.handleUserSubmit(name)} />;
     }
-};
+}
 
 export default App;
